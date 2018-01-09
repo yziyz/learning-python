67b78e4edb58fa21846b38214f93c8dd6efdef32
袁臻 <zenuor@gmail.com>
2018-01-09 10:59:28 +0800

    添加注释

--- opencv/read-image.py
+++ opencv/read-image.py
@@ -1,7 +1,11 @@
 import cv2
 
 if __name__ == '__main__':
-    img = cv2.imread('180px-Beethoven.jpg', 0)
+    # 以灰度模式读取图片"180px-Beethoven.jpg"
+    img = cv2.imread('180px-Beethoven.jpg', cv2.IMREAD_GRAYSCALE)
+    # 显示
     cv2.imshow('image', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # 等待"ESC"键，关闭所有窗口
+    k = cv2.waitKey(0)
+    if k == 27:
+        cv2.destroyAllWindows()
