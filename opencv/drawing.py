ea4f22f1e58a8e94f2c4ca62d20ae3319e95c567
袁臻 <zenuor@gmail.com>
2018-01-09 15:19:13 +0800

    修改方法

--- opencv/drawing.py
+++ opencv/drawing.py
@@ -2,11 +2,12 @@ import cv2
 import numpy as np
 
 
-def wait_esc_key():
+def show_and_wait_esc(img: np.ndarray):
     """
     等待"ESC"键，关闭所有窗口
     :return: None
     """
+    cv2.imshow("image", img)
     k = cv2.waitKey(0)
     if k == 27:
         cv2.destroyAllWindows()
@@ -22,14 +23,12 @@ if __name__ == '__main__':
     # 绘制椭圆
     cv2.ellipse(img, (256, 256), (100, 50), 0, 0, 180, 255, -1)
     # 绘制圆形
-    img = cv2.circle(img, (256, 256), 32, (0, 255, 255), thickness=-1)
+    cv2.circle(img=img, center=(256, 256), radius=32, color=(0, 0, 255), thickness=-1)
     # 绘制多边形
-    pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], dtype=np.int32)
-    pts = pts.reshape((-1, 1, 2))
-    img = cv2.polylines(img, [pts], isClosed=True, color=(0, 255, 255))
+    pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], dtype=np.int32).reshape((-1, 1, 2))
+    cv2.polylines(img, [pts], isClosed=True, color=(0, 255, 255))
     # 绘制文字
     font = cv2.FONT_HERSHEY_PLAIN
     cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
     # 显示图片
-    cv2.imshow("image", img)
-    wait_esc_key()
+    show_and_wait_esc(img)
